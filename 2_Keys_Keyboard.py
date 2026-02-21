c6f8ca05f748e4dd6d27dd64bfbe0f919e156333
Katyayani Verma <112198478+Katyayani736@users.noreply.github.com>
2026-02-21 12:28:19 +0530

    test

--- 2_Keys_Keyboard.py
+++ 2_Keys_Keyboard.py
@@ -3,10 +3,10 @@ class Solution:
         if n == 1:
             return 0
         steps = 0
-        factor = 2
+        factor=2
         while n > 1:
             while n % factor == 0:
                 steps += factor
                 n //= factor
-            factor += 1
+            factor+=1
         return steps
